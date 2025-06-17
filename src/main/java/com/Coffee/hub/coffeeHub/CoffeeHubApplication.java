cfd674e93b06d64aac838625f9744f8aa4581ca2
vanarasis <vanarasi.sainath07@gmail.com>
2025-06-17 13:53:33 +0530

    17-06

---
+++ src/main/java/com/Coffee/hub/coffeeHub/CoffeeHubApplication.java
@@ -0,0 +1,13 @@
+package com.Coffee.hub.coffeeHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeHubApplication.class, args);
+	}
+
+}
